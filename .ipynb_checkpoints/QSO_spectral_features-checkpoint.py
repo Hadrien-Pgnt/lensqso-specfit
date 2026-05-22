Added continuum flux calculation + fixed GH flux + minor changes

.ipynb_checkpoints/QSO_spectral_features-checkpoint.py
@@ -24,13 +24,13 @@ QSO_narrow_doublets = {'NeV': [(3346.79, 3426.85), 2.73], # Cleri et al. (2023),
                        }
 
 ### Rest frame wavelengths (in vacuum) of QSO emission lines (mostly broad lines), in Å
-QSO_single_lines = {'Halpha': 6564.7,
+QSO_single_lines = {'Halpha': 6564.6,
                     'Hbeta': 4862.7,
-                    'Pa_eps': 9548.6, #HI (8->3)
+                    'Pa_eps': 9548.8, #HI (8->3)
                     'Heps': 3971.2,
-                    'Pa_zeta': 9231.6, #HI (9->3)
-                    'Pa_eta': 9017.4, #HI (10->3)
-                    'NII_8986': 8986.18,
+                    'Pa_zeta': 9232.2, #HI (9->3)
+                    'Pa_eta': 9017.8, #HI (10->3)
+                    'NII_8988': 8988.64,
                     'OII': 3728.48, #narrow line (in reality a doublet but two lines are unresolved), cf Vanden Berk et al. (2001) 
                     }
 
@@ -93,6 +93,13 @@ class PolynomialContinuum(SpectrumComponent):
             resp_M_t.append(vandermonde[:,self.degree-i])
         return resp_M_t 
 
+    def get_total_flux(self, coeffs, lambda_min=None, lambda_max=None):
+        '''The total flux only makes sense over a range [lambda_min, lambda_max], use the same range as the data by default.'''
+        lamNorm_min = (lambda_min-self.lambda_c)/self.lambda_scale if lambda_min is not None else -1
+        lamNorm_max = (lambda_max-self.lambda_c)/self.lambda_scale if lambda_max is not None else 1
+        integ_pol = np.polyint(coeffs)
+        return (np.polyval(integ_pol, lamNorm_max) - np.polyval(integ_pol, lamNorm_min)) * self.lambda_scale
+
 
 class PowerLawContinuum(SpectrumComponent):
     ''' Fixed parameters:
@@ -114,6 +121,12 @@ class PowerLawContinuum(SpectrumComponent):
 
     def make_transposed_response_matrix(self, lamRest, beta):
         return [(lamRest/self.lambda_c)**beta]
+
+    def get_total_flux(self, amp, beta, lambda_min=None, lambda_max=None):
+        '''The total flux only makes sense over a range [lambda_min, lambda_max].'''
+        if lambda_min is None or lambda_max is None:
+            raise ValueError("The total continuum flux depends on the wavelength range: you need to specify that range for a power-law.")
+        return amp * self.lambda_c / (beta+1) * ((lambda_max/self.lambda_c)**(beta+1) - (lambda_min/self.lambda_c)**(beta+1))
 
 class GaussHermiteLine(SpectrumComponent):
     '''Represents an individual emission line, with Gauss-Hermite functions to represent beyond-Gaussian line profiles.
@@ -178,38 +191,18 @@ class GaussHermiteLine(SpectrumComponent):
         '''Returns the integrated line flux of a line represented by a Gauss-Hermite series with coefficients coeffsHermite = [c0, c1, c2,...]
         '''
         total_flux = coeffsHermite[0] #count order 0 separately (scipy convention is factorial2(-1)=0)
-        for i in range(1, len(coeffsHermite)):
-            if (i%2) == 0 : #only even orders matter
-                total_flux += coeffsHermite[i]*factorial2(i-1)/np.sqrt(factorial(i))
-        return total_flux
-
-
-    def make_flux_old(self, lamRest, dlam, width, coeffsHermite):
-        '''DEPRECATED. Old formulation (not the usual Gauss-Hermite functions)'''
-        
-        lambda_c = self.lambda_rest + dlam
-        lamNorm = (lamRest-lambda_c)/(np.sqrt(2)*width) #wavelength array normalized by Gaussian profile
-        
-        #Use the physicist's Hermite polynomials, and normalize the Hermite-Gaussian functions in order to have the integral = coeffsHermite[0] for order 0  - such that it is the integrated flux in the line (the integral is =0 for higher orders)
         
         if not(self.fix_c1c2_to_zero):  #c1, c2 are free parameters so are already in coeffsHermite
-            return 1 / (np.sqrt(2*np.pi)*width) * hermval(lamNorm, coeffsHermite) * np.exp(-lamNorm**2)
+            for i in range(1, len(coeffsHermite)):
+                if (i%2) == 0 : #only even orders matter
+                    total_flux += coeffsHermite[i]*factorial2(i-1)/np.sqrt(factorial(i))
+                    
         else: #fix c1=c2=0 -> coeffsHermite only includes c0, c3, ...
-            coeffsHermite_extended = np.hstack([coeffsHermite[0], [0,0], coeffsHermite[1:]]) 
-            return 1 / (np.sqrt(2*np.pi)*width) * hermval(lamNorm, coeffsHermite_extended) * np.exp(-lamNorm**2)
+            for i in range(3, 2+len(coeffsHermite)):
+                if (i%2) == 0 : #only even orders matter
+                    total_flux += coeffsHermite[i-2]*factorial2(i-1)/np.sqrt(factorial(i))
 
-    def make_transposed_response_matrix_old(self, lamRest, dlam, width):
-        '''DEPRECATED. Old formulation (not the usual Gauss-Hermite functions)'''
-        resp_M_t = []
-        lambda_c = self.lambda_rest + dlam
-        lamNorm = (lamRest-lambda_c)/(np.sqrt(2)*width) #wavelength array normalized by Gaussian profile
-        monomial = np.zeros(self.degree+1)
-        for i in range(self.degree+1):      
-            monomial *=0
-            monomial[i] = 1
-            if not(self.fix_c1c2_to_zero) or not(1<=i<=2):
-                resp_M_t.append( 1/(np.sqrt(2*np.pi)*width) * hermval(lamNorm, monomial) * np.exp(-lamNorm**2))
-        return resp_M_t
+        return total_flux
 
 
 class GaussHermiteLineRelShape(GaussHermiteLine):
@@ -356,38 +349,6 @@ class NarrowDoublet(SpectrumComponent):
 
     def get_total_flux(self,  **kwargs_values):
         return (1+self.line_ratio)*self.line1.get_total_flux(**kwargs_values)
-
-class NarrowDoubletGaussHermiteRelShape(NarrowDoublet):
-    '''DEPRECATED. 
-    Same as NarrowDoublet(type='GaussHermite'), but this time the Hermite coefficients with order >=1 (in relcoeffsHermite) are treated as non-linear parameters (expressed relative to order 0) to allow for the shape parameters to be shared between different images. Only the overall scaling *amp* (i.e., the order 0 coefficient) is treated as a linear parameter.
-    '''
-    
-    def __init__(self, name, kwargs_init={}):
-        super().__init__(name, 'GaussHermite', kwargs_init)
-        self.linear_params = ['amp']
-        self.nonlinear_params = ['dlam', 'width', 'relcoeffsHermite']
-        self.priors['relcoeffsHermite'] = [0, 0.1, -1, 1] 
-        self.fix_c1c2_to_zero = self.line1.fix_c1c2_to_zero
-        
-    def make_flux(self, lamRest, amp, dlam, width, relcoeffsHermite):
-        #assume the same line profile for both lines in the doublet, but with an amplitude scaled by the line ratio
-        Hermcoeffs = np.hstack([[1], relcoeffsHermite]) #add the 0th order coefficient (=1) only 
-                                                        #(c1=c2=0 will be added by the calls to line1 and line2)
-        return amp*(self.line1.make_flux(lamRest, dlam, width, Hermcoeffs) + self.line_ratio*self.line2.make_flux(lamRest, dlam, width, Hermcoeffs))
-
-    def make_transposed_response_matrix(self, lamRest, dlam, width, relcoeffsHermite):
-        #assume the same line profile for both lines in the doublet, but with an amplitude scaled by the line ratio
-        Hermcoeffs = np.hstack([[1], relcoeffsHermite]) #add the 0th order coefficient (=1) only 
-                                                    #(c1=c2=0 will be added by the calls to line1 and line2)
-        return [self.line1.make_flux(lamRest, dlam, width, Hermcoeffs) + self.line_ratio*self.line2.make_flux(lamRest, dlam, width, Hermcoeffs)]
-    
-    def set_priors(self, priors): 
-        #update prior of this class and also of the two child instances of Line()
-        super().set_priors(priors)
-        priors_copy = copy.deepcopy(priors)
-        priors_copy.pop('relcoeffsHermite', None) #remove coeffsHermite from dictionary to avoid raising an error (not a parameter in Line())
-        self.line1.set_priors(priors)
-        self.line2.set_priors(priors)
 
 
 class FeII_Vis_TemplateLines(SpectrumComponent):

multi_spec_fitter.py
@@ -207,7 +207,7 @@ class MCMCSampler(Optimizer):
          ax.plot(dist_normed, label="logL", color="k", linewidth=2)
 
     
-    def flux_ratio_posterior(self, feature, ref_image='default', samples=None, n_samples=1000, n_burn_add=0):
+    def flux_ratio_posterior(self, feature, ref_image='default', samples=None, n_samples=1000, n_burn_add=0, continuum_range=None):
          '''Calculates a posterior probability distribution on the flux ratios for a given feature, from model parameter samples.
          
          Inputs:
@@ -217,10 +217,11 @@ class MCMCSampler(Optimizer):
          *samples: list of parameter samples. If None, uses the MCMC chain stored in the class (requires a run_mcmc beforehand).
          *n_samples: number of random re-samples of the chain to estimate the posterior
          *n_burn_add: number of samples to ignore at the start of the chain, to avoid re-sampling parts of the chain where the MCMC had not yet converged.
+         *continuum_range: wavelength range over which to integrate the continuum flux. If None, use the same range as the data for the first image
          
          Outputs:
-         * feature_fluxratios: array of flux-ratio values
-         * list(fr_names.keys()): list of labels for the flux ratios
+         * fluxratios_array: array of flux-ratio values for the chosen feature.
+         * list(fr_indices.keys()): list of labels for the flux ratios
          '''
          if samples is None:
              samples = self.samples_mcmc
@@ -234,64 +235,62 @@ class MCMCSampler(Optimizer):
          num_samples_tot = len(samples[:, 0])
          subsample = n_burn_add + np.random.choice(a=num_samples_tot-n_burn_add, size=n_samples)
 
-         fr_names = {}
+         fr_indices = {}
          index = 0
          if ref_image is not None:
              assert (ref_image in self.multi_spec.spec_dict.keys())
              for image_name in self.multi_spec.spec_dict: 
                  if not(image_name==ref_image):
-                     fr_names['f_'+image_name+'/f_'+ref_image] = index
+                     fr_indices['f_'+image_name+'/f_'+ref_image] = index
                      index+=1
          else:
              for image1 in self.multi_spec.spec_dict: 
                  for image2 in self.multi_spec.spec_dict:
                      if not(image1==image2):
-                         fr_names['f_'+image1+'/f_'+image2] = index
+                         fr_indices['f_'+image1+'/f_'+image2] = index
                          index+=1
               
          #Array that will contain the flux ratios for each sample
          fluxratios_array = np.zeros((n_samples, index))         
                  
-         # find position of relevant amplitude parameter in the list of linear params 
-         # (depending on whether the Hermite coefficients are treated as linear or non-linear the integrated flux is named differently)
-         image_name = self.multi_spec.param_handler.ref_image
-         try:
-             k = self.multi_spec.spec_dict[image_name].lin_param_handler.linear_param_list.index(feature + '_amp')
-         except ValueError:
-             k = self.multi_spec.spec_dict[image_name].lin_param_handler.linear_param_list.index(feature + '_coeffsHermite0')
-                 
          for i in range(n_samples):
-            
-             #sample the non-linear parameter
-             array_nonlinear_free_params = samples[subsample[i]]
 
-             kwargs_nonlinear_mult = self.multi_spec.param_handler.array2kwargs_nonlinear(array_nonlinear_free_params)
-             feature_fluxes = {}
+             kwargs_values_mult_sample = {}
+             feature_fluxes_sample = {}
+            
+             #sample the non-linear parameters from the MCMC chain
+             array_nonlinear_free_params_sample = samples[subsample[i]]
+             
+             # convert from array to non-linear kwargs
+             kwargs_nonlinear_mult_sample = self.multi_spec.param_handler.array2kwargs_nonlinear(array_nonlinear_free_params_sample)
+             
              for image_name in self.multi_spec.spec_dict:
+                 #get likelihood mask
                  mask = self.kwargs_lik['mask_dict'][image_name] if image_name in self.kwargs_lik['mask_dict'] else np.ones_like(
-                     self.multi_spec.spec_dict[image_name].lambda_array)
+                     self.multi_spec.spec_dict[image_name].lambda_array) 
+                 
+                 #solve for linear parameters (get MLE + covariance estimate)
                  lin_params_MLE, lin_params_cov = self.multi_spec.spec_dict[image_name].solve_linear_params(
-                     kwargs_nonlinear=kwargs_nonlinear_mult[image_name],mask_array=mask)
+                     kwargs_nonlinear=kwargs_nonlinear_mult_sample[image_name],mask_array=mask)
                 
                  #sample from conditional distribution (linear params knowing non-linear params)
                  lin_params_sample = np.random.multivariate_normal(lin_params_MLE, lin_params_cov, size=1)[0]
 
-                 feature_fluxes[image_name] = lin_params_sample[k]
-                
-             #calculate the flux-ratios
-             if ref_image is not None:
-                 for image_name in self.multi_spec.spec_dict: 
-                     if not(image_name==ref_image):
-                         index = fr_names['f_'+image_name+'/f_'+ref_image]
-                         fluxratios_array[i][index] = feature_fluxes[image_name]/feature_fluxes[ref_image]
-             else:
-                 for image1 in self.multi_spec.spec_dict: 
-                     for image2 in self.multi_spec.spec_dict:
-                         if not(image1==image2):
-                             index = fr_names['f_'+image1+'/f_'+image2]
-                             fluxratios_array[i][index] = feature_fluxes[image1]/feature_fluxes[image2]
+                 #add sampled linear values to dict of sampled non-linear values (for 1 image)
+                 kwargs_values_samples = self.multi_spec.spec_dict[image_name].lin_param_handler.add_linear_values_to_kwargs(lin_params_sample, 
+                                                                                                     kwargs_nonlinear_mult_sample[image_name])
+                 #add to dictionary with values for all images
+                 kwargs_values_mult_sample[image_name] = kwargs_values_samples
+
+             #calculate the flux ratios for that sample
+             flux_ratios_sample = self.multi_spec.get_flux_ratios_of_feature(kwargs_values_mult_sample, feature, 
+                                                                             ref_image=ref_image, continuum_range=continuum_range)
+
+             for fr_name in flux_ratios_sample:
+                 index = fr_indices[fr_name]
+                 fluxratios_array[i][index] = flux_ratios_sample[fr_name]
          
-         return fluxratios_array, list(fr_names.keys())   
+         return fluxratios_array, list(fr_indices.keys())   
 
 class FittingSequence():
 